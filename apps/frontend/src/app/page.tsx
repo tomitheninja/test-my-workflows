2a4aa2061c7e095ecca68d6e5ecee1b82f5d0813
Tamás Südi <suditomi+github@pm.me>
2024-10-30 18:04:15 +0100

    Async workflow

--- apps/frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { HelloWorld } from '@/components/hello-world';
-
-export default function Home() {
-  return (
-    <main className='flex items-center justify-center'>
-      <HelloWorld className='mt-10' />
-    </main>
-  );
-}
